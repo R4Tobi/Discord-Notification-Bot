19b57525c455a73befa1d9a04d9717da7d162125
Tobias Baake <62656419+R4Tobi@users.noreply.github.com>
2024-08-05 20:25:10 +0200

    updated logger.py

--- logger.py
+++ logger.py
@@ -1,9 +1,18 @@
 import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Logger:
+
+    def writeFile(self, msg):
+        with open(os.getenv("LOGPATH"), "a") as f:
+            f.write(msg + "\n")
 
     def getTime(self):
         return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
     def log(self, msg):
         print(f"\033[30m{self.getTime()} \033[94m{'INFO'}     \033[95m{'custom.bot'} \033[0m{msg}")
+        self.writeFile(f"{self.getTime()} INFO  custom.bot {msg}")
