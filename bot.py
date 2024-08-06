a0c886f660215624c503a5d909d568b735c3ff55
Tobias Baake <62656419+R4Tobi@users.noreply.github.com>
2024-08-06 10:12:54 +0200

    fixed logging

--- bot.py
+++ bot.py
@@ -36,31 +36,12 @@ async def on_message(message):
     logger.log(message.author.name + '@' + message.guild.name + ": " + message.content)
 
     if message.author == bot.user: 
-        return 
-
-    if message.content.startswith('$hello'): 
-        await message.channel.send('Hallo! ' + message.author.mention)
-
-    if message.content.startswith('$timeout'):
-        args = message.content.split(' ')
-        if len(args) == 3:
-            try:
-                user = int(args[1].replace('<@!', '').replace('>', ''))
-                timeout = int(args[2])
-                member = message.guild.get_member(user)
-                member.timeout(datetime.now() + timeout, reason=args[3])
-            except ValueError:
-                await message.channel.send('Invalid parameter.')
-        else:
-            await message.channel.send('Invalid number of arguments.')
+        return
     
 @bot.event
 async def on_voice_state_update(member, before, after):
     # Check if the user joined a voice channel
     if before.channel is None and after.channel is not None:
-        # Debugging output to verify event trigger
-        logger.log(f'{member.name} joined {after.channel.name}. Members: {[m.name for m in after.channel.members]}')
-        
         # Check if the voice channel is empty before the user joins
         if len(after.channel.members) == 1:
             # Get the 'notifications' role
@@ -70,14 +51,16 @@ async def on_voice_state_update(member, before, after):
                 channel = discord.utils.get(member.guild.text_channels, name='notifications')  # Replace 'general' with your channel name
                 if channel:
                     await channel.send(f'{role.mention}, {member.mention} has joined the voice channel {after.channel.mention}.')
-                    logger.log(f'{member.name} started a voice chat in {after.channel.name}.')
+                    logger.log(f'{member.name} started a voice channel in {after.channel.name}.')
                 else:
                     logger.error('Text channel "notifications" not found.')
             else:
                 logger.error('Role "notifications" not found.')
         else:
-            logger.log(f'{member.name} joined a voice chat.')
-    else:
-        logger.log(f'{member.name} left a voice channel.')
+            logger.log(f'{member.name} joined the voice chaannel {after.channel.name}.')
+    elif before.channel is not None and after.channel is None:
+        logger.log(f'{member.name} left voice channel {before.channel.name}. Members: {[m.name for m in after.channel.members]}')
+    elif before.channel is not None and after.channel is not None:
+        logger.log(f'{member.name} switched from {before.channel.name} to {after.channel.name}. Members: {[m.name for m in after.channel.members]}')
 
 bot.run(os.getenv('DISCORD_TOKEN'))
